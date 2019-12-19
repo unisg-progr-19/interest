7efa4217b57173afb7c2d6526aec5f430614c135
Kirill Müller <krlmlr+r@mailbox.org>
2019-12-19 17:20:01 +0100

    ggplotly

--- script/11-plot.R
+++ script/11-plot.R
@@ -6,3 +6,19 @@ tbl_interest <- readRDS("data/interest.rds")
 
 tbl_interest %>%
   plot_time_series()
+
+
+tbl_interest %>%
+  plot_time_series() %>%
+  plotly::ggplotly()
+
+p <-
+  tbl_interest %>%
+  tsbox::ts_long() %>%
+  ggplot(aes(x = time, y = value, color = id)) +
+  geom_line() +
+  labs(x = "Time", y = "Value", color = "Series")
+
+p
+
+plotly::ggplotly(p)
